313ee371117bf5970995f20349a506638e772ffd
zedguyyer <153858744+zedguy@users.noreply.github.com>
2026-05-27 11:59:44 -0600

    chump

---
+++ UI.lua
@@ -0,0 +1,146 @@
+local repo = "https://raw.githubusercontent.com/deividcomsono/Obsidian/main/"
+
+if not getgenv().LinoriaCache then
+    getgenv().LinoriaCache = {
+        Library = loadstring(game:HttpGet(repo .. "Library.lua"))(),
+        ThemeManager = loadstring(game:HttpGet(repo .. "addons/ThemeManager.lua"))(),
+        SaveManager = loadstring(game:HttpGet(repo .. "addons/SaveManager.lua"))()
+    }
+end
+
+local Library = getgenv().LinoriaCache.Library
+local ThemeManager = getgenv().LinoriaCache.ThemeManager
+local SaveManager = getgenv().LinoriaCache.SaveManager
+
+local Options = Library.Options
+local Toggles = Library.Toggles
+local Window = Library:CreateWindow({
+    Title = "Darkest Hours Menu",
+    Center = true,
+    AutoShow = true,
+    ShowCustomCursor = true,
+})
+local Tabs = {
+    Main = Window:AddTab("Main", "sword"),
+    Visuals = Window:AddTab("Visuals", "eye"),
+    ["UI Settings"] = Window:AddTab("UI Settings", "settings"),
+}
+
+local MainGroupBox = Tabs.Main:AddLeftGroupbox("Main")
+local UtilityGroupBox = Tabs.Main:AddRightGroupbox("Utilities")
+
+MainGroupBox:AddToggle("ESPToggle", {
+    Text = "ESP",
+    Default = false,
+    Callback = function(Value)
+        print("ESP:", Value)
+    end,
+}):AddKeyPicker("ESPKey", {
+    Default = "C",
+    SyncToggleState = true,
+    Mode = "Toggle",
+    Text = "ESP",
+    NoUI = false
+})
+
+MainGroupBox:AddToggle("ReviveToggle", {
+    Text = "Auto-Revive",
+    Default = false,
+    Callback = function(Value)
+        print("Auto-Revive:", Value)
+    end,
+}):AddKeyPicker("ReviveKey", {
+    Default = "H",
+    SyncToggleState = true,
+    Mode = "Toggle",
+    Text = "Auto-Revive",
+    NoUI = false
+})
+
+MainGroupBox:AddToggle("ExitToggle", {
+    Text = "Auto-Exit",
+    Default = false,
+    Callback = function(Value)
+        print("Auto-Exit:", Value)
+    end,
+}):AddKeyPicker("ExitKey", {
+    Default = "M",
+    SyncToggleState = true,
+    Mode = "Toggle",
+    Text = "Auto-Exit",
+    NoUI = false
+})
+
+MainGroupBox:AddToggle("VoteToggle", {
+    Text = "Spam Votes",
+    Default = false,
+    Callback = function(Value)
+        print("Spam Votes:", Value)
+    end,
+}):AddKeyPicker("VoteKey", {
+    Default = "B",
+    SyncToggleState = true,
+    Mode = "Toggle",
+    Text = "Spam Votes",
+    NoUI = false
+})
+
+UtilityGroupBox:AddToggle("TrackerToggle", {
+    Text = "Show Entity Overlay",
+    Default = true,
+    Callback = function(Value)
+        print("Entity Overlay:", Value)
+    end,
+})
+
+UtilityGroupBox:AddDivider()
+
+UtilityGroupBox:AddButton({
+    Text = "Print Spawned Entities",
+    Func = function()
+        print("Print Spawned Entities Clicked")
+    end,
+})
+
+UtilityGroupBox:AddButton({
+    Text = "Force Revive All",
+    Func = function()
+        print("Force Revive All Clicked")
+    end,
+})
+
+local MenuGroup = Tabs["UI Settings"]:AddLeftGroupbox("Menu")
+
+MenuGroup:AddToggle("KeybindMenuOpen", {
+    Default = false,
+    Text = "Open Keybind Menu",
+    Callback = function(Value)
+        Library.KeybindFrame.Visible = Value
+    end,
+})
+
+MenuGroup:AddLabel("Menu bind")
+    :AddKeyPicker("MenuKeybind", {
+        Default = "RightShift",
+        NoUI = true,
+        Text = "Menu keybind"
+    })
+
+MenuGroup:AddButton("Unload", function()
+    Library:Unload()
+end)
+
+Library.ToggleKeybind = Options.MenuKeybind
+
+ThemeManager:SetLibrary(Library)
+SaveManager:SetLibrary(Library)
+
+SaveManager:IgnoreThemeSettings()
+SaveManager:SetIgnoreIndexes({ "MenuKeybind" })
+
+ThemeManager:SetFolder("DarkestHours")
+SaveManager:SetFolder("DarkestHours/main")
+
+SaveManager:BuildConfigSection(Tabs["UI Settings"])
+ThemeManager:ApplyToTab(Tabs["UI Settings"])
+SaveManager:LoadAutoloadConfig()
