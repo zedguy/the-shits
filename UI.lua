f4e37b52105f5c8eb568305d478073b71f3ac33d
zedguyyer <153858744+zedguy@users.noreply.github.com>
2026-06-03 15:58:13 -0600

    speedtestw2354

--- UI.lua
+++ UI.lua
@@ -64,7 +64,7 @@ function ClientSpeedEffect:Start()
                 
                 speedStack:AddModifier(self._stackName, function(modifierData)
                     -- Formula matches the decompiler: Base * (1 + Level / 3)
-                    modifierData.Output = (modifierData.Output or 1) * (1 + self.SpeedLevel / 33)
+                    modifierData.Output = (modifierData.Output or 1) * (1 + ((self.SpeedLevel - 1) * 5))
                     return false
                 end, 5, true)
                 
